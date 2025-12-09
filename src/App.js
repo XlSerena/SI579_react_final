e5780ab51e97f7cbf14ea2901f6f4222df7f941c
XlSerena <xulan_serena@sjtu.edu.cn>
2025-12-08 23:24:34 -0500

    1

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import TestComponent from "./components/TestComponent";
-
-const Comp = () => {
-  return <p>This is a comp component</p>
-}
+import ProblemWrapper from "./ProblemWrapper";
 
 function App() {
-  return (
-    <div className="App" id='root'>
-            <TestComponent />
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Comp />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <h1>UMSI 579 - Final Deliverable (React)</h1>
+            <h3>Lan Xu</h3>
+            <ProblemWrapper name='Increment'><TestComponent/></ProblemWrapper>
+        </>
+    );
 }
 
 export default App;
--- src/components/TestComponent.js
+++ src/components/TestComponent.js
@@ -1,5 +1,18 @@
+import {useState} from 'react';
+
+// Updating the screen by using State: https://beta.reactjs.org/learn#updating-the-screen
+// Hooks: https://beta.reactjs.org/learn#using-hooks (specifically useState)
+// Listening to events: https://beta.reactjs.org/learn#responding-to-events
 const TestComponent = () => {
-  return <p><b>Another test component</b></p>
+  // @todo, update the button with a new number each time it is clicked.
+  // @todo, in addition to the "short" version, prof will show a version that uses
+  // previousValue, too. One of the two can be commented out.
+  const [count, setCount] = useState(0);
+  return <button
+      onClick={() => setCount(prev => prev + 1)}
+      className='btn btn-primary btn-lg'>
+    Number of clicks: {count}
+  </button>
 }
 
 export default TestComponent;
